2dbe350a4143006477dd4cbe511f2f2923a31c25
Nicolas Gagne <ngagne966@gmail.com>
2025-02-21 21:23:56 -0500

    Initial commit

---
+++ MoveHandler.h
@@ -0,0 +1,23 @@
+#ifndef MOVEHANDLER_H
+#define MOVEHANDLER_H
+#include <AccelStepper.h>
+
+extern float actual_el;
+extern float actual_az;
+extern const float precision_limit;
+extern int step_per_turn;
+extern const int stepMainPin;
+extern const int dirMainPin;
+extern AccelStepper mainStepper;
+extern const int dirSecondPin;
+extern const int limitELPin; // X.DIR Switch limite for Elevation
+extern const int limitAZPin; // Y.DIR Switch limite for azumute
+
+
+
+// Function prototypes
+void resetRotator();
+bool resetElevation(); 
+bool resetAzimute(); 
+
+#endif // MOVEHANDLER_H
